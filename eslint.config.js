02f82469101453fcfedd808f237ab9da3575e392
Ash hassan <ashrfhassan890@gmail.com>
2024-06-15 18:10:31 +0300

    updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,18 @@
-module.exports = {
-  env: {
-    node: true,
-    browser: true,
-    es2021: true,
-    jest: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:storybook/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    camelcase: 'error',
-    'spaced-comment': 'error',
-    quotes: ['error', 'single'],
-    'no-duplicate-imports': 'error',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {},
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
+const nodePlugin = require('eslint-plugin-n');
+
+module.exports = [
+  nodePlugin.configs['flat/recommended-script'],
+  {
+    languageOptions: {
+      sourceType: 'module',
     },
   },
-};
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
+  {
+    ignores: ['node_modules', 'dist', 'build', 'storybook-static'],
+  },
+];
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const { ids } = require('webpack');
 const webpack = require('webpack');
